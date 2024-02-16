d5416e5c87b7df77c21cb0b914d1a04792840642
tanaymalik <tanaymalik06@gmail.com>
2024-02-16 12:54:44 +0530

    configure width logic and add hover and color functionality

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const gridContainer = document.querySelector(".grid-container")
 
 
+noOfSquares = 10;
+totalNoOfSquares = noOfSquares*noOfSquares;
 
-
-
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= totalNoOfSquares; i++) {
     const newSquare = document.createElement("div")
 
     newSquare.style.height = '48px';
     newSquare.style.width = '48px';
     newSquare.style.border = '1px solid black'
-
-
-
-    
-    
-   
+    newSquare.classList.add("new-square");
 
 
     gridContainer.appendChild(newSquare);  
@@ -27,5 +22,23 @@ let size = window.getComputedStyle(firstSquare).getPropertyValue('height');
 
 size = parseFloat(size);
 
-let width = (size + 2) * 16 + 28;
+let width = (size + 2) * noOfSquares + (noOfSquares*2);
 gridContainer.style.width = width + 'px';
+
+
+const allSquares = document.querySelectorAll(".new-square");
+
+
+
+function hover(square) {
+    square.style.backgroundColor = "black";
+
+}
+function unhover(square) {
+    square.style.backgroundColor = "white";
+}
+
+allSquares.forEach(square => {
+    square.addEventListener("mouseenter", () => hover(square));
+    square.addEventListener("mouseleave", () => unhover(square));
+})
